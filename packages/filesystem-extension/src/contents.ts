0e0c431f778120839db88fb88525046a9217b202
Marten Richter <marten.richter@tu-berlin.de>
2025-01-02 13:17:41 +0000

    Increase nbformat version

--- packages/filesystem-extension/src/contents.ts
+++ packages/filesystem-extension/src/contents.ts
@@ -206,7 +206,7 @@ export class FailsContents implements IContents {
     metadata: {
       orig_nbformat: 4
     },
-    nbformat_minor: 4,
+    nbformat_minor: 5,
     nbformat: 4,
     cells: []
   };
